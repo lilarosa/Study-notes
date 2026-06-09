ae865ee80ec1c9a45a3bf42d5af736e338fa869b
Tao <tt9898daa@gmail.com>
2026-06-09 13:23:12 +0200

    Add fields and constructor to Artikel class

--- Java/Workspace/objektorietierung/src/uebung_kalssendiagramm_Artikel/Artikel.java
+++ Java/Workspace/objektorietierung/src/uebung_kalssendiagramm_Artikel/Artikel.java
@@ -1,5 +1,17 @@
 package uebung_kalssendiagramm_Artikel;
 
 public class Artikel {
-
+private String artikelName;
+	private double Grosse;
+	
+	public Artikel(String artikelName, double Grosse) {
+		this.artikelName = artikelName;
+		this.Grosse = Grosse;
+	}
+	public void setArtikelName( String artikelName) {
+		this.artikelName = artikelName;
+		
+	}
+	public void anzeigen() {
+		System.out.println("Artikel: " + artikelName + "Grosse: " + Grosse);}
 }
